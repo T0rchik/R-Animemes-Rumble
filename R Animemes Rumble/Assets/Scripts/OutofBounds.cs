0d3dec1a86d43438dce596a715444b53233abf57
Torchik <the3rdr0b0t@gmail.com>
2018-07-20 16:07:45 -0400

    Basic Movement
    Base Commit:
    Two possible players: 3D cube & 2D Sprite
    Player can jump and double jump

---
+++ R Animemes Rumble/Assets/Scripts/OutofBounds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutofBounds : MonoBehaviour {
+
+    //private Vector2 center = gameobject.transform.position;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ R Animemes Rumble/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : PhysicsObject {
+
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpTakeOffSpeed;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > 0)
+            {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        if (Input.GetButtonDown("Jump") && doubleJump && !grounded)
+        {
+            velocity.y = jumpTakeOffSpeed;
+            doubleJump = false;
+        }
+
+        targetVelocity = move * maxSpeed;
+    }
+}
